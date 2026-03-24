fd248483a355f896aac631f3ec1ab0e51226e467
reeee1104 <reeee1027@gmail.com>
2026-03-23 23:36:39 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,22 @@
+<?php
+session_start();
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h2>Login Form</h2>
+
+<form method="post" action="authenticate.php">
+    Username: <input type="text" name="username" required><br><br>
+    Password: <input type="password" name="password" required><br><br>
+    Remember Me: <input type="checkbox" name="remember"><br><br>
+    <input type="submit" value="Login">
+</form>
+
+</body>
+</html>
